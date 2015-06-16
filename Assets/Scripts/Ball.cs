9b3f146653ffcdbeb8d63f016105254247c71c60
Yaroslava Rusakova <yarus7777@gmail.com>
2015-06-16 13:10:58 +0300

    ball and platform moving

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -2,13 +2,26 @@
 
 public class Ball : MonoBehaviour {
 
+    private Vector2 ballInitialForce;
+    private bool IsRunning;
+
+
 	// Use this for initialization
 	void Start () {
-	
+        ballInitialForce = new Vector2(1500.0f, 1500.0f);	
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+
+    public void SendBall()
+    {
+        if (!IsRunning)
+        {
+            rigidbody2D.AddForce(ballInitialForce);
+            IsRunning = true;
+        }
+    }
 }
---
+++ Assets/Scripts/InputDetector.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class InputDetector : MonoBehaviour
+{
+
+    public Ball ball;
+    public Platform platform;
+    private Camera camera;
+    private float yPosition;
+
+	void Start () {
+        camera = Camera.main;
+	    yPosition = platform.GetComponent<RectTransform>().anchoredPosition.y;
+	}
+	
+	void Update () {
+	
+	}
+
+    public void OnScreenClick ()
+    {
+        ball.SendBall();
+    }
+
+    private Vector2 pointerOffset;
+
+    public void OnDown ()
+    {
+        Vector2 clickPosition = camera.ScreenToWorldPoint(UnityEngine.Input.mousePosition);
+        pointerOffset = clickPosition - platform.GetComponent<RectTransform>().anchoredPosition;
+    }
+
+    public void OnDrag()
+    {
+        Vector2 pos = camera.ScreenToWorldPoint(UnityEngine.Input.mousePosition);
+        platform.GetComponent<RectTransform>().anchoredPosition = new Vector2((pos - pointerOffset).x, yPosition);
+    }
+}
